1ed4a2789c6bb6370660ba953d884f0f2d846ddd
Arina-Putyatina <popowa.arina@yandex.ru>
2022-09-04 14:52:53 +0300

    init commit

---
+++ src/main/java/ru/netology/springbootconditional/config/JavaConfig.java
@@ -0,0 +1,23 @@
+package ru.netology.springbootconditional.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.netology.springbootconditional.service.DevProfile;
+import ru.netology.springbootconditional.service.ProductionProfile;
+import ru.netology.springbootconditional.service.SystemProfile;
+
+@Configuration
+public class JavaConfig {
+    @Bean
+    @ConditionalOnProperty(name = "netology.profile.dev", havingValue = "true")
+    public SystemProfile devProfile() {
+        return new DevProfile();
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "netology.profile.dev", havingValue = "false")
+    public SystemProfile prodProfile() {
+        return new ProductionProfile();
+    }
+}
---
+++ src/main/java/ru/netology/springbootconditional/service/DevProfile.java
@@ -0,0 +1,8 @@
+package ru.netology.springbootconditional.service;
+
+public class DevProfile implements SystemProfile {
+    @Override
+    public String getProfile() {
+        return "Current profile is dev";
+    }
+}
